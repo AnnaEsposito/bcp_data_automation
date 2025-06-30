46605b807e07bb9e4c63d94ff6c9c4576738dfdf
Anna Paula Esposito <101672244+AnnaEsposito@users.noreply.github.com>
2025-06-30 08:57:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,6 @@ file_path = "C:/Users/Anna Esposito/Desktop/bcp_data_automation/data/raw_excel/A
 df_long_cuadro_6= transform_frame_6(file_path)
 df_long_cuadro7 =transform_cuadro_7(file_path)
 
-# Ver si el dataframe tiene valores nulos en fecha o valor
-#print(df_long_cuadro_6[df_long_cuadro_6["Fecha"].isna() | df_long_cuadro_6["Valor"].isna()])
-
-# Para contar cuantos registros son nulos
-
-#df_long_cuadro_6 = df_long_cuadro_6.dropna(subset=["Fecha", "Valor"])
-#print(df_long_cuadro_6[df_long_cuadro_6["Fecha"].isna() | df_long_cuadro_6["Valor"].isna()].shape[0])
-#Convertir en Excel
-#df_long_cuadro_6.to_excel("df_long_cuadro_6.xlsx", index=False)
 # Cargar a la BD
 cargar_hechos_cuadro6(df_long_cuadro_6, id_indicador=2, id_fuente=1, id_unidad=1)
 cargar_hechos_cuadro7(df_long_cuadro7, id_indicador=3, id_fuente=1, id_unidad=1)
